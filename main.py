9ef42df48f1f27ed0be81ed0668b3b1cb0db76d7
OORibeiro <168047562+OORibeiro@users.noreply.github.com>
2026-05-13 19:28:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ def gerar_atividades():
     """
 
     resposta = client.chat.completions.create(
-        model="google/gemma-2-9b-it:free",
+        model="openai/gpt-oss-20b:free",
         messages=[
             {
                 "role": "user",
